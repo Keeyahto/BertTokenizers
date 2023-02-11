61b0527b6b10818a77b6bdd920d33f175fd64375
Yaroslav Mironov <ya.mironov14@gmail.com>
2023-02-12 00:34:03 +0300

    Изменение названия SbertTokenizer на SbertLargeTokenizer

---
+++ src/SbertLargeTokenizer.cs
@@ -0,0 +1,14 @@
+﻿using BERTTokenizers.Base;
+
+namespace BERTTokenizers
+{
+    public class SbertLargeTokenizer
+        : UncasedTokenizer
+    {
+        public SbertLargeTokenizer() : base("./Vocabularies/sbert.txt")
+        {
+        }
+    }
+
+     
+}
